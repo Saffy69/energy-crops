e82403a6e1fd1b98a340b3df808725c56863e5ea
unknown <safaldhungana7@gmail.com>
2022-06-11 05:10:59 -0400

    about update

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,20 +1,8 @@
 const teamData = [
   {
-    id: 9,
-    name: "Som Chettri",
-    post: "Founder",
-    image:
-      "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/286092839_3224433694504717_1903716145447971679_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=t_Xw8jvBy5gAX89bkZc&tn=A6zgxIs3oLBd3Z1y&_nc_ht=video.fktm1-1.fna&oh=03_AVJy2L3noX5iYNwIMV8WUbTJCcssopn8hQ1FLVfj9tozhA&oe=62C9B1A5",
-    about:
-      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
-    facebook: "https://facebook.com",
-    twitter: "https://twitter.com",
-    mess: "https://messenger.com",
-  },
-  {
     id: 1,
     name: "Deepak Dhungana",
-    post: "Ui Developer",
+    post: "Founder",
     image:
       "https://video.fktm1-1.fna.fbcdn.net/v/t31.18172-8/19693727_1373183289385861_5403691789727290836_o.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a4a2d7&_nc_ohc=E-YRDij5oTcAX-XmzgF&_nc_ht=video.fktm1-1.fna&oh=00_AT_knUFC62mh7h3r57YT3GDVsGkmKWQzgCH6oZ7pA5ggLQ&oe=62C5B894",
     about:
@@ -24,9 +12,33 @@ const teamData = [
     mess: "https://messenger.com",
   },
   {
+    id: 6,
+    name: "Dev Nayaran Dhungana",
+    post: "Founder",
+    image:
+      "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/286467749_539162591069672_2138589507437705277_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=e4OwWlmFvmgAX8YNZWP&_nc_ht=video.fktm1-1.fna&oh=03_AVIrDdoPyySlMUyz37OWBfF8QapcFm3kMiW6KU_AQ4N9mg&oe=62C54AEC",
+    about:
+      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    mess: "https://messenger.com",
+  },
+  {
+    id: 5,
+    name: "kalyan Kattel",
+    post: "Founder",
+    image:
+      "https://video.fktm1-2.fna.fbcdn.net/v/t39.30808-6/252782846_102211005616105_3440623811260721618_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5RFeGPOZoboAX-PNLeo&_nc_ht=video.fktm1-2.fna&oh=00_AT_JGAqLggpoHcL8xz8IQJII3Dhb-UKsNdITNtNiISsrZA&oe=62A5BFAA",
+    about:
+      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    mess: "https://messenger.com",
+  },
+  {
     id: 2,
     name: "Ujwal Bhattrai",
-    post: "Ui Developer",
+    post: "Investor",
     image:
       "https://video.fktm1-1.fna.fbcdn.net/v/t39.30808-6/275298775_1829226147468644_3644190104148625840_n.jpg?stp=dst-jpg_p843x403&_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=hgzJ1N2a3EMAX_LvDs2&_nc_ht=video.fktm1-1.fna&oh=00_AT8YAIp18JNPauHIZk194I3HQGZGOYnU_wWRtwK5OUAngw&oe=62A49A91",
     about:
@@ -38,7 +50,7 @@ const teamData = [
   {
     id: 3,
     name: "Uvraj Poudel",
-    post: "Ui Developer",
+    post: "Investor",
     image:
       "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/286508635_430593201910465_1253805780900316_n.jpg?stp=dst-jpg_s180x540&_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=okq56Iss614AX9i3598&_nc_oc=AQlYba2xG7h6ot--n0__y14dkx3958R66JGQ_8-pWC87CTKTj9-l2y_beGJV7a5D1jI&_nc_ht=video.fktm1-1.fna&oh=03_AVL8VYV4kJX-gDd8-Cs88i951GurYBTjXIjdAHfRvlMJzw&oe=62C5C29A",
     about:
@@ -50,7 +62,7 @@ const teamData = [
   {
     id: 4,
     name: "Ram Dhungana",
-    post: "Ui Developer",
+    post: "Investor",
     image:
       "https://video.fktm1-2.fna.fbcdn.net/v/t1.6435-9/67389383_2352096758204705_1771808493615972352_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=K8CWE1LTYXIAX86jFL9&_nc_ht=video.fktm1-2.fna&oh=00_AT-_tkbrjg0rNcdIY2mo5IzhQObR8rQ_Wv_0fd_Gg47Yzg&oe=62C461CB",
     about:
@@ -60,33 +72,9 @@ const teamData = [
     mess: "https://messenger.com",
   },
   {
-    id: 5,
-    name: "kalyan Kattel",
-    post: "Ui Developer",
-    image:
-      "https://video.fktm1-2.fna.fbcdn.net/v/t39.30808-6/252782846_102211005616105_3440623811260721618_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=5RFeGPOZoboAX-PNLeo&_nc_ht=video.fktm1-2.fna&oh=00_AT_JGAqLggpoHcL8xz8IQJII3Dhb-UKsNdITNtNiISsrZA&oe=62A5BFAA",
-    about:
-      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
-    facebook: "https://facebook.com",
-    twitter: "https://twitter.com",
-    mess: "https://messenger.com",
-  },
-  {
-    id: 6,
-    name: "Dev Nayaran Dhungana",
-    post: "Ui Developer",
-    image:
-      "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/286467749_539162591069672_2138589507437705277_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=e4OwWlmFvmgAX8YNZWP&_nc_ht=video.fktm1-1.fna&oh=03_AVIrDdoPyySlMUyz37OWBfF8QapcFm3kMiW6KU_AQ4N9mg&oe=62C54AEC",
-    about:
-      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
-    facebook: "https://facebook.com",
-    twitter: "https://twitter.com",
-    mess: "https://messenger.com",
-  },
-  {
     id: 7,
     name: "Roshan Budathoki",
-    post: "Ui Developer",
+    post: "Investor",
     image:
       "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/285881651_1161206648054816_417286858198965556_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=jKzmGksMSnwAX8EGPUC&_nc_ht=video.fktm1-1.fna&oh=03_AVK093DVaGX1MmvzQhbhsYVK0N8wcznXLbR6D6NGL2Q8RA&oe=62C7F7A8",
     about:
@@ -98,9 +86,21 @@ const teamData = [
   {
     id: 8,
     name: "Suhang Limbu",
-    post: "Ui Developer",
+    post: "Investor",
     image:
       "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/285379263_1340146293176029_8749521219962432638_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=LkRS12RQwnsAX9XfEDR&_nc_ht=video.fktm1-1.fna&oh=03_AVLdNoHvhXGp4Y0z_Blho4mOB727XfG5z7eYZ18ZwPbVnA&oe=62CADB6A",
+    about:
+      "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    mess: "https://messenger.com",
+  },
+  {
+    id: 9,
+    name: "Som Chettri",
+    post: "Investor",
+    image:
+      "https://video.fktm1-1.fna.fbcdn.net/v/t1.15752-9/286092839_3224433694504717_1903716145447971679_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=t_Xw8jvBy5gAX89bkZc&tn=A6zgxIs3oLBd3Z1y&_nc_ht=video.fktm1-1.fna&oh=03_AVJy2L3noX5iYNwIMV8WUbTJCcssopn8hQ1FLVfj9tozhA&oe=62C9B1A5",
     about:
       "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware",
     facebook: "https://facebook.com",
